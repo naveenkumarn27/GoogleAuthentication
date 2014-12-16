a5235d1469b76bd56ffb6fa85ef870c3913b4733
Naveen Kumar <naveenkumarn2@gmail.com>
2014-12-16 05:32:15 -0500

    Google authentication in Android

---
+++ app/src/main/java/com/android/npgraphics/googleauthentication/Authenticator/GoogleAuthenticator.java
@@ -0,0 +1,15 @@
+package com.android.npgraphics.googleauthentication.Authenticator;
+
+
+import android.app.Activity;
+
+public class GoogleAuthenticator {
+
+    private Activity mActivity;
+
+    public GoogleAuthenticator (Activity activity) {
+        this.mActivity = activity;
+    }
+
+
+}
